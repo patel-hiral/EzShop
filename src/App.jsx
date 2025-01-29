888c34be25c3a332bd85d7f2fbc82a18e7d4fa2d
mdalmamunit427 <mdalmamunit427@gmail.com>
2025-01-29 22:32:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl text-center'>Hello There!</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
