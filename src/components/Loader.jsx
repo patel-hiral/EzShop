cd3c34ecb4e0c76be35dff1ac495f76031e76676
Hiral Patel <hiral.patel@differenzsystem.com>
2025-02-19 13:09:10 +0530

    Removed Create Portal From The Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,13 @@
 import { Loader2 } from "lucide-react";
 import React from "react";
-import { createPortal } from "react-dom";
+
 function Loader() {
-  return createPortal(
-    <div className="fixed inset-0 bg-[#00000083] backdrop-blur-sm flex items-center justify-center z-50">
-      <div className="mx-auto flex flex-col items-center justify-center">
-        <Loader2 className="animate-spin text-white w-10 h-10" />
-        <p className="text-center text-white">Please Wait...</p>
-      </div>
-    </div>,
-    document.getElementById("loader")
-  );
+  return <div className="fixed inset-0 bg-[#00000083] backdrop-blur-sm flex items-center justify-center z-50">
+    <div className="mx-auto flex flex-col items-center justify-center">
+      <Loader2 className="animate-spin text-white w-10 h-10" />
+      <p className="text-center text-white">Please Wait...</p>
+    </div>
+  </div>
 }
 
 export default Loader;
